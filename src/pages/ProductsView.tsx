08d3eeabb624470016cfe6c6f0cc80ce5a4f61f1
Luiz <luizgustavofcsg@hotmail.com>
2020-11-08 13:35:15 -0300

    built behavior to load mock data from backend REST API

--- src/pages/ProductsView.tsx
+++ src/pages/ProductsView.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import backend from '../services/backend';
 
 function ProdutsView() {
+
+    const [products, setProducts] = useState<String>('')
+
+    useEffect(() => {
+        backend.get('/products').then((response) => {
+            setProducts(JSON.stringify(response.data))
+        })
+    }, [])
+
     return (
         <div className="App">
-            <h1>DeltaOPT</h1>
+            <h1>DeltaOPT </h1>
+            <p>{products}</p>
         </div>
     )
 }
