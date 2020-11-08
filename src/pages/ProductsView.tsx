58d5ec464d2bdbfc61b177e80ea5a9a2381a57da
Luiz <luizgustavofcsg@hotmail.com>
2020-11-08 18:41:27 -0300

    Created component for the application navigation bar

--- src/pages/ProductsView.tsx
+++ src/pages/ProductsView.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Button } from '@material-ui/core'
 
 import backend from '../services/backend';
-
+import AppBar from '../components/AppBar/AppBar'
 
 
 function ProdutsView() {
@@ -17,7 +17,7 @@ function ProdutsView() {
 
     return (
         <div className="App">
-            <h1>DeltaOPT </h1>
+            <AppBar />
             <p>{products}</p>
         
             <Button variant="contained" color="primary">
